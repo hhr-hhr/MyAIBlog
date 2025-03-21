8551ab48cb284df08353cc7259c9999c68fcc0a7
z36003 <lejie.chen@weaver.com.cn>
2025-03-21 19:41:11 +0800

    更新字体

--- static/js/typewriter.js
+++ static/js/typewriter.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   if (typewriterElement) {
     // 初始化Typed.js
     new Typed('.Typewriter__wrapper', {
-      strings: ['Build My Programming Insights'], // 替换为您想要显示的文本
+      strings: ['USEAI','Build My Passive Income'], // 替换为您想要显示的文本
       typeSpeed: 50,          // 打字速度
       backSpeed: 30,          // 删除速度
       backDelay: 3000,        // 删除前的延迟
